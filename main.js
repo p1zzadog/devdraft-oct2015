e899e5ca482eb9060cc51758b0161ba81d6cc5d2
Steven A Moody <steve.moody2@gmail.com>
2015-11-13 16:15:15 -0700

    created time table arrays to easily reference in any visitor itinerary

--- main.js
+++ main.js
@@ -32,44 +32,109 @@ var park = (function(tA, wlkArr){
     return p;
 })(totalAttractions, walkingTimes);
 
-park.forEach(function(attraction){
+console.log('park: ', park);
+
+
+// strategy: create two(counterclockwise and clockwise) 2-dimensional arrays representing all possible paths
+// outer array index represents origin of travel
+// inner array index represents destination, value represents travel time
+// i.e. ccTimeTable[3][2] --> 18 (minutes)
+
+
 //    cc table
-    var ccTable = [];
+var ccTimeTable = (function(){
 
-});
+    var ccTemp = [];
+    var fullCircle = function(){
+        return park.reduce(function(ticktock, attraction){
+            return ticktock + attraction.ccTime;
+        },0);
+    };
 
-//var visitorTotal = prompt('Enter the number of visitors');
-//
-//var Visitor = function (numberOfAttractions, attractionsItinerary) {
-//    this.numberOfAttractions = numberOfAttractions;
-//    this.attractionsItinerary = attractionsItinerary;
-//};
-//
-//var allVisitors = (function(vT, pk){
-//    var num;
-//    var attractions;
-//    var arr = [];
-//    var toInt = function(string){return parseInt(string)};
-//    var parkItemIndex = function(attractionIndex){
-//        return pk.findIndex(function(attraction){
-//            return attraction.index===attractionIndex;
-//        });
-//    };
-//    var getAttraction = function(parkItemIndex){return p[parkItemIndex]};
-//    for (var i=0; i<tot; i++){
-//        num = parseInt(prompt("how many attractions does this guest visit?"));
-//        attractions = prompt("which attractions?")
-//            .split(' ')
-//            .map(toInt)
-//            .map(parkItemIndex)
-//            .map(getAttraction);
-//        arr.push(new Visitor(num, attractions));
-//    }
-//    return arr;
-//})(visitorTotal, park);
-//
-//console.log('park: ', park);
-//console.log('allVisitors: ', allVisitors);
+    var ccRouteNoWrap = function(start, stop){
+        var ticktock=0;
+        for (var i = start; i < stop; i++) {
+            ticktock += park[i].ccTime;
+        }
+        return ticktock;
+    };
+
+    var ccRouteWrap = function(start, stop){
+        var ticktock=0;
+        for (var i = stop; i < start; i++) {
+            ticktock += park[i].ccTime;
+        }
+        return ticktock;
+    };
+
+    for (var origin = 0; origin<park.length; origin++){
+
+        var ccTempPrime = [];
+        for (var destination = 0; destination<park.length; destination++){
+
+            ccTempPrime.push((function() {
+
+                if (origin === destination) return fullCircle();
+
+                if (origin <= destination) return ccRouteNoWrap(origin, destination);
+
+                if (origin > destination) return fullCircle() - ccRouteWrap(origin, destination);
+
+
+            })());
+        }
+        ccTemp.push(ccTempPrime);
+    }
+    return ccTemp;
+})();
+
+// easiest to produce CW array by transposing CC array
+var transpose = function(array){
+    var arr = [];
+    return arr = array[0].map(function(col, i){
+        return array.map(function(row){
+            return row[i];
+        });
+    });
+};
+var cwTimeTable = transpose(ccTimeTable);
+
+console.log('ccTimeTable: ', ccTimeTable);
+console.log('cwTimeTable: ', cwTimeTable);
+
+
+var visitorTotal = prompt('Enter the number of visitors');
+
+var Visitor = function (attrNumber, attrItinerary) {
+    this.attrNumber = attrNumber;
+    this.attrItinerary = attrItinerary;
+};
+
+var allVisitors = (function(vT, pk){
+    var num;
+    var attractions;
+    var arr = [];
+    var toInt = function(string){return parseInt(string)};
+    var parkItemIndex = function(attractionIndex){
+        return pk.findIndex(function(attraction){
+            return attraction.index===attractionIndex;
+        });
+    };
+    var getAttraction = function(parkItemIndex){return p[parkItemIndex]};
+    for (var i=0; i<tot; i++){
+        num = parseInt(prompt("how many attractions does this guest visit?"));
+        attractions = prompt("which attractions?")
+            .split(' ')
+            .map(toInt)
+            .map(parkItemIndex)
+            .map(getAttraction);
+        arr.push(new Visitor(num, attractions));
+    }
+    return arr;
+})(visitorTotal, park);
+
+console.log('park: ', park);
+console.log('allVisitors: ', allVisitors);
 //
 //var computeOutput = function(aV, pk, tA){
 //
